1665e5208198d8b3f4f3632b2157d04973fba2e9
dinhson2000 <75033827+dinhson2000@users.noreply.github.com>
2022-03-24 21:04:40 +0700

    first commit

---
+++ he_thong_tinh_diem_ptit_nhom11/src/test/java/com/qlsv/ptit/tinhdiemptit/TinhDiemPtitApplicationTests.java
@@ -0,0 +1,17 @@
+package com.qlsv.ptit.tinhdiemptit;
+
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinhDiemPtitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
